a63d8b0b2c3bf739ade5d91a61d7f77ec7205261
Nolan Carlisle <143767678+NCarlisle24@users.noreply.github.com>
2025-09-14 19:26:58 -0700

    added express server functionality

---
+++ server.js
@@ -0,0 +1,28 @@
+/* ------------------------------------------- Imports ------------------------------------------- */
+const express = require('express'); // Local web server hosting
+const path = require('path'); // File path utilities
+
+/* ------------------------------------------- Setup ------------------------------------------- */
+const app = express(); // create Express instance
+const PORT = 3000;
+const URL = 'http://localhost:' + PORT;
+const PAGES_DIR = path.join(__dirname, 'src/pages');
+
+app.use(express.static(PAGES_DIR)); // Work only with files in 'public' folder
+
+/* ------------------------------------------- Routes ------------------------------------------- */
+
+// Home page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(PAGES_DIR, '/index.html'));
+})
+
+// catch all other routes and return 404
+// app.use((req, res) => {
+    
+// })
+
+/* ------------------------------------------- Start server ------------------------------------------- */
+app.listen(PORT, () => {
+    console.log("Server running on " + URL);
+});
