d2ab9e93828d45a79047a7702f5361ef5746927f
EthanSNAU <eqs4@nau.edu>
2025-10-02 13:40:22 -0700

    Add files via upload

---
+++ public/pages/scripts/romanNumGen.js
@@ -0,0 +1,53 @@
+// this script handles random generation 
+
+// name: genRomanNumeral
+// desc: generates a random Roman numeral between I and VII
+// Inputs: none
+// Outputs: romanNumeral
+// Contributors: Chris
+
+function genRomanNumeral() {
+    // roman numerals from I to VII
+    const romanNumerals = ["I", "II", "III", "IV", "V", "VI", "VII"];
+
+    let result = "";
+
+    //loop 4 times to build the string
+    for(let i = 0; i < 4; i++){
+        // generate a random index between 0 and 6
+        const index = Math.floor(Math.random() * romanNumerals.length);
+
+        // get the Roman numeral
+        const romanNumeral = romanNumerals[index];
+
+        // add it to the result string
+        result += romanNumeral + " " ;
+    }
+
+
+    // display it on the webpage
+    const displayElement = document.getElementById('romanNum');
+    displayElement.textContent = result.trim();
+    
+    //get rid of any whitespace
+    complete_string = result.trim();
+    
+    // return
+    return complete_string;
+}
+
+
+// name: getKey
+// desc: allows user to select what key they want to be in
+// Inputs: keyName
+// Outputs: keyDisplay
+// Contributors: Ethan
+
+function getKey(keyName)
+{
+    // creates a display element for updating the webpage
+    const display = document.getElementById('keyDisplay');
+
+    //updates the display with the selected key name
+    display.textContent = `key: ${keyName}`;
+}
