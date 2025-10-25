15ddc73dabb6a40b001863e6a53dae24cde5645b
Nolan Carlisle <143767678+NCarlisle24@users.noreply.github.com>
2025-10-25 04:32:00 -0700

    Fixed file paths in testing files

--- tests/integration/romanNumGen.test.js
+++ tests/integration/romanNumGen.test.js
@@ -1,4 +1,4 @@
-const FILE_PATH = "../src/public/scripts/romanNumGen.js";
+const FILE_PATH = "../../src/public/scripts/romanNumGen.js";
 const { getRandomInt, genRomanNumeral, genInversions, clearInversions, NUM_CHORDS } = require(FILE_PATH);
 
 // empty for now
