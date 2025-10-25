9e60cf01e04fedc2b7758b9a55707dfe17922d54
Nolan Carlisle <143767678+NCarlisle24@users.noreply.github.com>
2025-10-25 04:29:49 -0700

    Sorted tests by unit vs. integration

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
     testEnvironment: "jsdom",
+    testMatch: ["**/tests/**/*.test.js"] // only runs tests in the tests folder
 }
---
+++ tests/integration/romanNumGen.test.js
@@ -0,0 +1,4 @@
+const FILE_PATH = "../src/public/scripts/romanNumGen.js";
+const { getRandomInt, genRomanNumeral, genInversions, clearInversions, NUM_CHORDS } = require(FILE_PATH);
+
+// empty for now
